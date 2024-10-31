f88185c42e627e8fe45b792f0520471c51caee30
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2024-11-01 00:19:30 +0600

    Add files via upload

--- react-knowledge-cafe/src/App.jsx
+++ react-knowledge-cafe/src/App.jsx
@@ -1,24 +1,35 @@
 import { useState } from 'react'
 import './App.css'
-import Blogs from './components/Header/Blogs/Blogs'
-import Bookmarks from './components/Header/Bookmarks/Bookmarks'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
-  const [bookmarks,setBookmarks]=useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
+  const [readingTime, setReadingTime] = useState(0)
 
-  const handleAddToBookmark=blog=>{
-    console.log('bookmark adding soon')
+  const handleAddToBookmark = blog => {
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
+  }
+
+  const handleMarkAsRead = (id, time) =>{
+    const newReadingTime = readingTime + time;
+    setReadingTime(newReadingTime);
+    // remove the read blog from bookmark
+    // console.log('remove bookmark', id)
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
+    setBookmarks(remainingBookmarks);
   }
 
   return (
     <>
-      
-   <Header></Header>
-   <div className='md:flex max-w-7xl mx-auto'>
-    <Blogs></Blogs>
-    <Bookmarks></Bookmarks>
-   </div>
+      <Header></Header>
+      <div className='md:flex max-w-7xl mx-auto'>
+        <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+        <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
+      </div>
+
     </>
   )
 }
